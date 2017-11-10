977452bbcbaab9ee6a927a6e72eb50c04b18cf4a
becca <r.alexis.tolentino@gmail.com>
2017-11-10 14:11:31 -0600

    configure build config

--- src/main/java/com/codeup/blog/springbootblog/SpringbootBlogApplication.java
+++ src/main/java/com/codeup/blog/springbootblog/SpringbootBlogApplication.java
@@ -2,10 +2,14 @@ package com.codeup.blog.springbootblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringbootBlogApplication extends SpringBootServletInitializer {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootBlogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
