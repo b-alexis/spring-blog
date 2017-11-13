0f9403c2c876272d673ddb46abffed73224e38c8
becca <r.alexis.tolentino@gmail.com>
2017-11-13 11:14:16 -0600

    Add missing @Controller annotations

--- src/main/java/com/codeup/blog/springbootblog/controllers/UsersController.java
+++ src/main/java/com/codeup/blog/springbootblog/controllers/UsersController.java
@@ -4,11 +4,13 @@ import com.codeup.blog.springbootblog.models.User;
 import com.codeup.blog.springbootblog.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UsersController {
     private UsersRepository repository;
     private PasswordEncoder encoder;
